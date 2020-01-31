279c2b4aea5f44e08a9155876c67d2330efa0d06
Emeka Mkpume <emeka.e.mkpume@outlook.com>
2020-01-31 11:49:40 -0500

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sub(int.Parse("21"), 42 / 2));
+        }
+        static int sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
